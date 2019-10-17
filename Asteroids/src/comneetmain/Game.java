9b216795d56b05b35436dc6d4af726c6f2dd5f84
matteivc <matheuspc@id.uff.br>
2019-10-17 13:23:24 -0300

    first commit

---
+++ Asteroids/src/comneetmain/Game.java
@@ -0,0 +1,34 @@
+package comneetmain;
+
+import com.badlogic.gdx.ApplicationListener;
+
+/**
+ *
+ * @author tetel
+ */
+public class Game implements ApplicationListener {
+    
+    public void create() {}
+    /*metodo que cria a imagem por meio de recursos gráficos */
+    
+    
+    
+    public void render () {}
+    /*metodo para alterar o tamanho da janela*/
+    
+    
+    
+    public void resize(int width, int heigth) {}
+    
+    
+    
+    public void pause() {}
+    
+    
+    public void resume() {}
+    
+    
+    public void dispose() {}
+    
+}
+   
